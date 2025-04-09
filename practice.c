9d72372984d4f62ea7fe185d4e125feb4bf6129d
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-09 19:31:16 +0300

    I was able to write my first switch statement in c

--- practice.c
+++ practice.c
@@ -246,7 +246,45 @@ int main(){
 
 
     // Trying out else if statement
+    int num = 0;
 
-    
+    if(num > 0){
+        printf("num is greater than 0\n");
+    } else if(num < 0){
+        printf("I think I have a negative number\n");
+    } else{
+        printf("num is just zero. No doubt\n");
+    };
+
+    // SWITCH STATEMENT with continue and break
+
+    int day = 3;
+    switch(day){
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+    }
+
+
+    // SWITCH STATEMENT WITHOUT BREAK BUT WITH CONTINUE
+
     return 0;
 }
