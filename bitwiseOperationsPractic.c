0c92858ecfb5f2108e103d22efc3efdcc4293a29
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-11 23:56:37 +0300

    was able to understand bit wise operation in a fairly good state

--- bitwiseOperationsPractic.c
+++ bitwiseOperationsPractic.c
@@ -17,7 +17,7 @@ int main(int argc, char* arg[]){
 
     // USING FLAGS INSTEAD (Here I want to represent multiple values with a bit of a variable to manage space.) Using a flag means using one variable for everything.
 
-    unsigned int flags = 2; // an integer has 4 (32 bits) bytes which means with a flag, I can fit in 32 features into it because four bytes is 32 bits.
+    unsigned int flags = 3; // an integer has 4 (32 bits) bytes which means with a flag, I can fit in 32 features into it because four bytes is 32 bits.
 
   if ((flags & 1) != 0){
     /// 0000 0000 0000 0000 0000 0000 0000 0010 & 0000 0000 0000 0000 0000 0000 0000 0001
@@ -31,7 +31,23 @@ int main(int argc, char* arg[]){
     printf("Second digit is enabled\n");
   }
 
-    
+
+  printf("\nWhen You want to set a specific bit use the or Operator. remember '0b' makes a number binary\n");
+
+  unsigned int m = 0b1 | 0b10 |0b10000000; 
+  // The result will be 0000 0000 0000 0000 0000 0000 1000 0011
+
+  if((flags & 0b10) != 0){
+    printf("Second digit is enabled\n");
+  }
+
+  if((flags & 0b1) != 0){
+    printf("First digit is enabled\n");
+  }
+
+  if((flags & 0b10000000) != 0){
+    printf("8th digit is enabled\n");
+  }
 
     return 0;
 }
