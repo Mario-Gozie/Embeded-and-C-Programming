e5f260a29680544dc49024a85bf499057c12f557
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-11 23:24:48 +0300

    getting my hand dirty with bit wise operations

--- bitwiseOperationsPractic.c
+++ bitwiseOperationsPractic.c
@@ -15,12 +15,16 @@ int main(int argc, char* arg[]){
     // }
 
 
-    // USING FLAGS INSTEAD (Here I want to represent multiple values with a bit of a variable to manage space.)
+    // USING FLAGS INSTEAD (Here I want to represent multiple values with a bit of a variable to manage space.) Using a flag means using one variable for everything.
 
     unsigned int flags = 2; // an integer has 4 (32 bits) bytes which means with a flag, I can fit in 32 features into it because four bytes is 32 bits.
 
   if ((flags & 1) != 0){
+    /// 0000 0000 0000 0000 0000 0000 0000 0010 & 0000 0000 0000 0000 0000 0000 0000 0001
+    // result is 0000 0000 0000 0000 0000 0000 0000 0000
     printf("1st features is enabled\n");
+  } else{
+    printf("value is equal to zero");
   }
 
     
